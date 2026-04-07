d2bd6e3e15fb58470c5e23707ed7933be63a9493
AmfibiaPC <nikaprrr312@gmail.com>
2026-04-07 10:39:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Render!"
-
-@app.route('/health')
-def health():
-    return "OK", 200
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+def home():
+    return render_template('index.html', words=words, topic=topic)
